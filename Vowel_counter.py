b67f181f7d77eaa4e64086d8093f9d733d483b65
ashif078 <aashialam727@gmail.com>
2026-01-15 23:01:36 +0530

    Create a simple vowel counter with the basic strins Methods

---
+++ Vowel_counter.py
@@ -0,0 +1,11 @@
+user = input("enter the line: " ).lower()
+
+print(user.count("a"))
+print(user.count("e"))
+print(user.count("i"))
+print(user.count("o"))
+print(user.count("u"))
+
+total=(user.count("a")+user.count("e")+user.count("i")+ user.count("o")+ user.count("u"))
+print(f"enter the name:{total}")
+
