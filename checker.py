1a7a6fee20cce69b6a07ecab6e17bc7efaa70649
ashif078 <aashialam727@gmail.com>
2026-01-18 22:57:29 +0530

    create a sample checker user,password

---
+++ checker.py
@@ -0,0 +1,10 @@
+user="ashif alam"
+password= 989898
+
+u=input("enter the user: ")
+p=int(input("enter the password: "))
+
+if(user==u and password==p):
+    print("successfully")
+else:
+    print("not successfully")    
