287e1b14d6c9ed89d1d1a5c7c3d420ece6f75e87
ashif078 <aashialam727@gmail.com>
2026-01-30 22:41:05 +0530

    create the complete To-do_list with the integration of database

--- database.py
+++ database.py
@@ -1,22 +1,60 @@
 import sqlite3
 
-file="task_list.db"
+file_name = "task_list.db"
 
 def get_connect():
-    return sqlite3.connect(file)
+    return sqlite3.connect(file_name)
 
 def create_table():
 
-    conn= get_connect()
-    cursor=conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS TASK(
-                   ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                   TASK TEXT NOT NULL,
-                   STATUS TEXT NOT NULL
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS task(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   task TEXT NOT NULL,
+                   status TEXT NOT NULL
                    )
 """)
-
+    
     conn.commit()
     conn.close()
 
-create_table()    
+def add_task(task):
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO task(task, status) VALUES (?,?)
+""",(task,"Pending"))
+    
+    conn.commit()
+    conn.close()
+
+def get_task():
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""SELECT * FROM task""")
+    data =  cursor.fetchall()
+
+    conn.close()
+    return data
+
+def delete_task(task_id):
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        DELETE FROM task WHERE id = ?
+""",(task_id, ))
+    
+    conn.commit()
+    deleted = cursor.rowcount
+    conn.close()
+
+    return deleted
