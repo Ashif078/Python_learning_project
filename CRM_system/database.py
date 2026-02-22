3d8012856f5f53b78993b4ebfc04317dfaafa023
ashif078 <aashialam727@gmail.com>
2026-02-22 23:51:47 +0530

    Design CRM scheme with client and interaction table

---
+++ CRM_system/database.py
@@ -0,0 +1,38 @@
+import sqlite3 
+
+file_name="client_database.db"
+
+def get_connect():
+    return sqlite3.connect(file_name)
+
+def create_table():
+    conn=get_connect()
+    cursor=conn.cursor()
+
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXIST clients(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT NIT NULL,
+                   PHONE_no TEXT NOT NULL,
+                   email TEXT NOT NULL UNIQUE,
+                   status TEXT NOT NULL CHECK(status IN("lead","contacted","converted","lost")),
+                   created_at TEXT DEFAULT CURRENT_TIMESTAMP)        
+                   
+                   
+""") 
+    
+    cursor.exexute(""""
+        CREATE TABLE IF NOT EXIST interaction(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   client_id INTEGER NOT NULL,
+                   note TEXT NOT NULL.
+                   interaction_type TEXT NOT NULL CHECK(interaction_type IN("call"."email","meeting")),
+                   created_at TEXT DEFALULT CURRENT_TIMESTAMP,
+                   FOREIGN KEY (client_id) REFERENCES client(id) )
+                   
+""")
+    
+    conn.comit()
+    conn.close()
+
